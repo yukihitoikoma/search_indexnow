c7f90ff6142747ed37ba92e778cf2ce4f5baaaf3
Yukihito Ikoma <yukihito.ikoma@gmail.com>
2022-02-18 15:53:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import configparser, requests, json, argparse
 config_ini = configparser.ConfigParser()
 config_ini.read('config.ini', encoding='utf-8')
 
-SEARCH_ENGINE_LIST = json.loads(config_ini.get('DEFAULT', 'SEARCH_ENGINE_LIST'))
+ENDPOINT = "https://api.indexnow.org/indexnow"
 DOMAIN = [config_ini['WEBSITE1']['DOMAIN'], config_ini['WEBSITE2']['DOMAIN']]
 API_KEY = [config_ini['WEBSITE1']['API_KEY'], config_ini['WEBSITE2']['API_KEY']]
 
@@ -18,13 +18,11 @@ urls = args.urls
 
 n = 0 if host == DOMAIN[0] else 1
 api = API_KEY[n]
-for engine in SEARCH_ENGINE_LIST:
-    ENDPOINT = f"https://{engine}/indexnow"
-    requests.post(
-        ENDPOINT, data=json.dumps({
-            "host" : host,
-            "key" : api,
-            "keyLocation" : f"https://{host}/{api}.txt",
-            "urlList" : urls
-        })
-    )
+requests.post(
+    ENDPOINT, data=json.dumps({
+        "host" : host,
+        "key" : api,
+        "keyLocation" : f"https://{host}/{api}.txt",
+        "urlList" : urls
+    })
+)
